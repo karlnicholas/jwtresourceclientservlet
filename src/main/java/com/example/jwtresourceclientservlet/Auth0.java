1c51a0c61eb812e81c75bbb80395797417ccaaa0
Karl Nicholas <karl.nicholas@outlook.com>
2023-06-29 12:32:08 -0400

    Initial commit

--- src/main/java/com/example/jwtresourceclientservlet/Auth0.java
+++ src/main/java/com/example/jwtresourceclientservlet/Auth0.java
@@ -3,9 +3,11 @@ package com.example.jwtresourceclientservlet;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@ConfigurationProperties("auth0")
+@ConfigurationProperties
+@PropertySource("classpath:auth0.properties")
 @Data
 public class Auth0 {
     private String tokenUri;
